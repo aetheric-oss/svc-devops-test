a63c70ebe0f1b86e83d313e230689bb410d168b1
owlot <95369289+owlot@users.noreply.github.com>
2023-01-03 11:46:33 +0100

    fix: module name in code

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,21 +1,21 @@
 //! gRPC server implementation
 
 ///module svc_storage generated from svc-storage.proto
-pub mod svc_template_rust {
+pub mod svc_devops_test {
     #![allow(unused_qualifications, missing_docs)]
     include!("grpc.rs");
 }
 
-use svc_template_rust::template_rust_rpc_server::{TemplateRustRpc, TemplateRustRpcServer};
-use svc_template_rust::{QueryIsReady, ReadyResponse};
+use svc_devops_test::devops_test_rpc_server::{DevopsTestRpc, DevopsTestRpcServer};
+use svc_devops_test::{QueryIsReady, ReadyResponse};
 use tonic::{transport::Server, Request, Response, Status};
 
 ///Implementation of gRPC endpoints
 #[derive(Debug, Default, Copy, Clone)]
-pub struct TemplateRustImpl {}
+pub struct DevopsTestImpl {}
 
 #[tonic::async_trait]
-impl TemplateRustRpc for TemplateRustImpl {
+impl DevopsTestRpc for DevopsTestImpl {
     /// Returns ready:true when service is available
     async fn is_ready(
         &self,
@@ -38,16 +38,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let full_grpc_addr = format!("[::]:{}", grpc_port).parse()?;
 
     let (mut health_reporter, health_service) = tonic_health::server::health_reporter();
-    let imp = TemplateRustImpl::default();
+    let imp = DevopsTestImpl::default();
     health_reporter
-        .set_serving::<TemplateRustRpcServer<TemplateRustImpl>>()
+        .set_serving::<DevopsTestRpcServer<DevopsTestImpl>>()
         .await;
 
     //start server
     println!("Starting gRPC server at: {}", full_grpc_addr);
     Server::builder()
         .add_service(health_service)
-        .add_service(TemplateRustRpcServer::new(imp))
+        .add_service(DevopsTestRpcServer::new(imp))
         .serve(full_grpc_addr)
         .await?;
     println!("gRPC server running at: {}", full_grpc_addr);
